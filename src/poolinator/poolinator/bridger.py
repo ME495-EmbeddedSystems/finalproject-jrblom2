6eb0e4a21561fac773e678a9ceca05eba6dd87d2
An Nguyen <annguyen2025@u.northwestern.edu>
2024-11-30 21:40:00 -0600

    depth image tbd

--- src/poolinator/poolinator/bridger.py
+++ src/poolinator/poolinator/bridger.py
@@ -24,7 +24,8 @@ class BridgeNode(Node):
     def __init__(self):
         super().__init__('bridge')
         self.bridge = CvBridge()
-        self.create_subscription(Image, 'image', self.opencv_process, 10)
+        self.create_subscription(Image, 'rgb_image', self.process_rgb_image, 10)
+        # self.create_subscription(Image, 'depth_image', self.process_depth_image, 10)
         self.pub = self.create_publisher(Image, 'new_image', 10)
         
         timer_period = 0.05 #secs
@@ -33,9 +34,12 @@ class BridgeNode(Node):
     def timer_callback(self):
         # self.get_logger().info('IN TIMERRRR')
         pass
+    
+    def process_depth_image(self, image):
+        # cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
+        pass
 
-
-    def opencv_process(self, image):
+    def process_rgb_image(self, image):
         """Draw a circle on the subscribed image and republish it to new_image."""
         cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
 
@@ -45,15 +49,13 @@ class BridgeNode(Node):
 
         detections = detector.detect(gray_image)
         if detections:
-            self.get_logger().info('DETECTED!!!')
             for detection in detections:
-                # self.get_logger().info(f'detection: {detection}') 
-                # self.get_logger().info(f'detection keys: {detection.keys()}')
+                self.get_logger().info(f'detection: {detection}') 
 
-                center = detection['center']  # nparray: [x, y]
+                center = detection['center']
                 center_x, center_y = int(center[0]), int(center[1])
 
-                # Draw a red dot at the center on the image
+                # draw a red dot at the center on the image
                 cv.circle(cv_image, (center_x, center_y), radius=10, color=(0, 0, 255), thickness=-1) 
 
         new_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8')
